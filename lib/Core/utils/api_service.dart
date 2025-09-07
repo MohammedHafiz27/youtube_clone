c59353147067ed52724c37a348a7bd7597e5954c
mohammed hafiz <mohammed.hafiz.2710@gmail.com>
2025-09-07 18:10:10 +0300

    api service done

---
+++ lib/Core/utils/api_service.dart
@@ -0,0 +1,17 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class ApiService {
+  static const String _baseUrl = "youtube-media-downloader.p.rapidapi.com";
+  static final Dio _dio = Dio();
+
+  Future<dynamic> getData(String endPoint) async {
+    final response = await _dio.get(
+      "$_baseUrl$endPoint",
+      options: Options(
+        headers: {'x-rapidapi-key': dotenv.env['APIKEY'], 'x-rapidapi-host': 'youtube-media-downloader.p.rapidapi.com'},
+      ),
+    );
+    return response.data;
+  }
+}
--- lib/Features/home_page/presentation/views/widgets/video_item.dart
+++ lib/Features/home_page/presentation/views/widgets/video_item.dart
@@ -8,7 +8,7 @@ class VideoItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 12),
+      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
