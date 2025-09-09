240863a21a4677f10ab09bff2bb1dfd7666992f2
mohammed hafiz <mohammed.hafiz.2710@gmail.com>
2025-09-09 16:32:36 +0300

    add MultiBlocProvider

--- lib/Features/video_details/presentation/views/video_details.dart
+++ lib/Features/video_details/presentation/views/video_details.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:youtube_clone/Core/utils/api_service.dart';
 import 'package:youtube_clone/Features/home_page/presentation/views/widgets/custom_app_bar.dart';
+import 'package:youtube_clone/Features/video_details/data/repos/comments_repo/comments_repo_imple.dart';
 import 'package:youtube_clone/Features/video_details/data/repos/video_repo/video_details_repo_imple.dart';
+import 'package:youtube_clone/Features/video_details/presentation/view_models/comments_cubit/comments_cubit.dart';
 import 'package:youtube_clone/Features/video_details/presentation/view_models/video_details_cubit/video_details_cubit.dart';
 import 'package:youtube_clone/Features/video_details/presentation/views/widgets/video_details_mobile_body.dart';
 
@@ -16,10 +18,9 @@ class VideoDetails extends StatelessWidget {
     return MultiBlocProvider(
       providers: [
         BlocProvider(
-          create: (context) =>
-              VideoDetailsCubit(VideoDetailsRepoImple(ApiService()))
-                ..getVideoDetails(videoId),
+          create: (context) => VideoDetailsCubit(VideoDetailsRepoImple(ApiService()))..getVideoDetails(videoId),
         ),
+        BlocProvider(create: (context) => CommentsCubit(CommentsRepoImple(ApiService()))..getComments(videoId)),
       ],
       child: Scaffold(
         appBar: AppBar(title: CustomAppBar()),
